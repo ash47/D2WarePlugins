f1ad7f07827fb5dc5edcb62de5d58ba299f91d8d
ash47 <ash.fourtyseven@gmail.com>
2013-12-16 22:29:48 +1100

    General Bug Fixes

--- plugins/No Fountain Camping.js
+++ plugins/No Fountain Camping.js
@@ -10,19 +10,20 @@ function onMapStart() {
 		
 		// Give this fountain a MKB
 		var truestrike = dota.createAbility(fountain, "item_monkey_king_bar");
-		dota.addNewModifier(fountain, truestrike, "modifier_item_monkey_king_bar", "item_monkey_king_bar", {});
+		dota.addNewModifier(fountain, truestrike, "modifier_item_monkey_king_bar", "item_monkey_king_bar", {}, fountain);
 		
 		// Give this fountain ursa's fury swipes
 		var swipes = dota.createAbility(fountain, 'ursa_fury_swipes');
 		swipes.netprops.m_iLevel = 4;
-		dota.addNewModifier(fountain, swipes, "modifier_ursa_fury_swipes", "ursa_fury_swipes", {});
+		dota.addNewModifier(fountain, swipes, "modifier_ursa_fury_swipes", "ursa_fury_swipes", {}, fountain);
 		
 		// Give this fountain SB's knock back
 		var bash = dota.createAbility(fountain, 'spirit_breaker_greater_bash');
 		bash.netprops.m_iLevel = 4;
-		dota.addNewModifier(fountain, bash, "modifier_spirit_breaker_greater_bash", "spirit_breaker_greater_bash", {});
+		dota.addNewModifier(fountain, bash, "modifier_spirit_breaker_greater_bash", "spirit_breaker_greater_bash", {}, fountain);
 	}
 	
 	// Preload ursa's particles
 	dota.loadParticleFile('particles/units/heroes/hero_ursa.pcf');
+	server.print('done')
 }
