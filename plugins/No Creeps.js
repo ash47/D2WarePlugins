18de4d900a18e438ec60b23f40328c1f70589ebc
ash47 <ash.fourtyseven@gmail.com>
2013-12-25 16:06:24 +1100

    Removed plugin mixer

--- plugins/No Creeps.js
+++
@@ -1,8 +0,0 @@
-// Hooks
-game.hook('OnGameFrame', onGameFrame);
-
-var cvCreepsNoSpawning = console.findConVar("dota_creeps_no_spawning");
-
-function onGameFrame(){
-	cvCreepsNoSpawning.setInt(1);
-}
--- plugins/No Hero Vision.js
+++
@@ -1,8 +0,0 @@
-// Hooks
-game.hook("Dota_OnUnitThink", onUnitThink);
-
-function onUnitThink(unit){
-	if(unit.isHero()){
-		dota.setUnitState(unit, dota.UNIT_STATE_NO_VISION, true);
-	}
-}
